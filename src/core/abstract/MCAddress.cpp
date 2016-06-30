a26cc2f7d9d995188b9d541fa1131f80fd1cd460
Dev Sanghani <dev_sanghani@yahoo.com>
2016-06-30 17:06:35 +0530

    Remove ssl/crypto flags + parse out commas in display names

--- src/core/abstract/MCAddress.cpp
+++ src/core/abstract/MCAddress.cpp
@@ -318,6 +318,9 @@ Object * Address::copy()
 
 void Address::setDisplayName(String * displayName)
 {
+    if (displayName != NULL) {
+        displayName->replaceOccurrencesOfString(MCSTR(","), MCSTR(""));
+    }
     MC_SAFE_REPLACE_COPY(String, mDisplayName, displayName);
 }
 
